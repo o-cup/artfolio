625321b33798477dbf207f8666e86320cb515014
jh22j9 <heeju.jeon@musinsa.com>
2024-02-18 19:42:04 +0900

    wip: 임시 포탈 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import GlobalStyle from "./styles/globalStyle";
+import Portal from "./components/Portal";
+import DetailView from "./components/DetailView";
 
 function App() {
 	return (
 		<>
 			<GlobalStyle />
 			<div className="App">
-				<p>Hello</p>
+				<p>메인</p>
+				<Portal isOpen={false} onClose={() => console.log("close")}>
+					<DetailView />
+				</Portal>
 			</div>
 		</>
 	);
