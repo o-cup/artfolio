5e5a531b096fe494ec4d63447293e87e6359a421
jh22j9 <heeju.jeon@musinsa.com>
2024-02-27 00:08:58 +0900

    feat: ExhibitionItemList 텍스트 추가

--- src/components/exhibitions/ExhibitionListItem.tsx
+++ src/components/exhibitions/ExhibitionListItem.tsx
@@ -1,21 +1,31 @@
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { DESKTOP_DEVICE, MOBILE_DEVICE } from "../../styles/theme";
+import Text from "../../shared/components/text";
 
 type Props = {
-	data: {
-		id: string;
-		posterUrl: string;
-	};
+	data: ExhibitionDataType;
 };
 
 const ExhibitionListItem = ({ data }: Props) => {
 	const navigate = useNavigate();
-	const { id, posterUrl } = data;
+	const { id, posterUrl, type, title, date, location } = data;
 
 	return (
 		<Styled.Wrap onClick={() => navigate(`/exhibitions/${id}`)}>
 			<Styled.Poster src={posterUrl} alt="poster" />
+			<Text typography="body2" color="gray" customStyle={{ fontSize: "12px" }}>
+				{type}
+			</Text>
+			<Text typography="body2" customStyle={{ marginBottom: "12px" }}>
+				{title}
+			</Text>
+			<Text typography="body2" customStyle={{ fontSize: "12px" }}>
+				{date}
+			</Text>
+			<Text typography="body2" customStyle={{ fontSize: "12px" }}>
+				{location}
+			</Text>
 		</Styled.Wrap>
 	);
 };
@@ -26,9 +36,15 @@ const itemWidth = "calc((100vw - (30px * 2) - (24px * 4)) / 5)";
 
 const Styled = {
 	Wrap: styled.li`
+		cursor: pointer;
+		line-height: 24px;
+
+		> span {
+			display: inline-block;
+		}
+
 		@media screen and (${DESKTOP_DEVICE}) {
 			width: ${itemWidth};
-			cursor: pointer;
 		}
 
 		@media screen and (${MOBILE_DEVICE}) {
@@ -37,6 +53,6 @@ const Styled = {
 	`,
 	Poster: styled.img`
 		width: 100%;
-		/* object-fit: cover; */
+		/* margin-bottom: 8px; */
 	`,
 };
