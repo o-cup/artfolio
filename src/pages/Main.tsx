fb6b464a0448fb4ff3189844aa91e0df6a99ca0e
jh22j9 <heeju.jeon@musinsa.com>
2024-03-01 16:05:02 +0900

    fix(works): 모바일 네이티브 뒤로가기 시 모달 닫기

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,15 +1,18 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { motion } from "framer-motion";
 import Layout from "../shared/components/layout";
 import MainList from "../components/main/MainList";
 import Motion from "../shared/components/motion";
+import { useModal } from "../context/ModalProvider";
 
 function Main() {
 	const navigate = useNavigate();
 
+	const { closeModal } = useModal();
+
 	useEffect(() => {
 		navigate("/works");
+		closeModal(); // words:id 내에서 모달 오픈 후 모바일 네이티브 뒤로가기 시 모달이 열린 상태인 문제 처리
 	}, []);
 
 	return (
