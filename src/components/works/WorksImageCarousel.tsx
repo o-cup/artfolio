c4b83f790e377e823d0fca8f0520ceadf894e7f6
jh22j9 <heeju.jeon@musinsa.com>
2024-03-01 16:28:23 +0900

    fix(works): 진입 시 캐러셀 첫번째 이미지에 포커스 고정

--- src/components/works/WorksImageCarousel.tsx
+++ src/components/works/WorksImageCarousel.tsx
@@ -1,6 +1,7 @@
-import { CarouselWrap } from "./styles/worksStyle";
+import React, { useRef, useEffect } from "react";
 import ModalViewer from "../../shared/components/modalViewer";
 import { useModal } from "../../context/ModalProvider";
+import { CarouselWrap } from "./styles/worksStyle";
 
 interface Props {
 	data: WorkDetailType;
@@ -8,20 +9,34 @@ interface Props {
 
 function WorksImageCarousel({ data }: Props) {
 	const { isModalOpen, openModal, setItemId } = useModal();
+	const carouselRef = useRef<HTMLDivElement>(null); // Create a ref for the carousel
 
 	const handleImgClick = (subId: number) => {
 		openModal();
 		setItemId(subId);
 	};
 
+	useEffect(() => {
+		if (carouselRef.current) {
+			carouselRef.current.scrollLeft = 0;
+		}
+	}, [data]);
+
 	return (
 		<>
-			<CarouselWrap>
-				{data.images.map((image) => {
-					const subId = data.images.indexOf(image) + 1;
+			<CarouselWrap ref={carouselRef}>
+				{data.images.map((image, index) => {
+					const subId = index + 1;
 
-					// eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
-					return <img key={image} src={image} alt={image} onClick={() => handleImgClick(subId)} />;
+					return (
+						<img
+							role="presentation"
+							key={subId}
+							src={image}
+							alt={`carousel-img-${subId}`}
+							onClick={() => handleImgClick(subId)}
+						/>
+					);
 				})}
 			</CarouselWrap>
 			{isModalOpen && <ModalViewer totalCount={data.imgCount} category="works" />}
